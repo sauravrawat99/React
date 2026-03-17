c2529dc83ef61795676752192e603c777ccc581f
sauravrawat99 <rawatsaurav145@gmail.com>
2026-03-17 17:11:00 +0530

    try to use effect learn

--- 02-State-project/src/App.jsx
+++ 02-State-project/src/App.jsx
@@ -1,13 +1,13 @@
-import React from "react";
 import Counter from "./Components/Counter";
-  import CounteCharacter from "./Components/CounteCharacter";
+import Button from "./Components/button";
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-400">
-      <Counter />
-      <CounteCharacter/>
-    </div>
+    <>
+      <div className="flex items-center justify-center h-screen bg-gray-400">
+        <Counter />
+      </div>{" "}
+    </>
   );
 };
 
